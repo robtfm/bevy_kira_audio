d32191a4aa2d2bab38c77515055e4b2add364861
robtfm <50659922+robtfm@users.noreply.github.com>
2023-07-26 15:06:41 +0100

    mod pub

--- src/audio_output.rs
+++ src/audio_output.rs
@@ -25,6 +25,7 @@ use std::collections::HashMap;
 /// This struct holds the [`AudioManager`] to play audio through. It also
 /// keeps track of all audio instance handles and which sounds are playing in which channel.
 pub struct AudioOutput<B: Backend = DefaultBackend> {
+    /// the kira audio manager
     pub manager: Option<AudioManager<B>>,
     instances: HashMap<Channel, Vec<Handle<AudioInstance>>>,
     channels: HashMap<Channel, ChannelState>,
