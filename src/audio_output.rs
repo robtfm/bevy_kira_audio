693f4bd6cfc88d8d747e28dcdb2025c1cead83d6
robtfm <50659922+robtfm@users.noreply.github.com>
2024-03-14 21:22:14 +0000

    actually pub manager

--- src/audio_output.rs
+++ src/audio_output.rs
@@ -25,7 +25,7 @@ use std::collections::HashMap;
 /// This struct holds the [`AudioManager`] to play audio through. It also
 /// keeps track of all audio instance handles and which sounds are playing in which channel.
 pub struct AudioOutput<B: Backend = DefaultBackend> {
-    manager: Option<AudioManager<B>>,
+    pub manager: Option<AudioManager<B>>,
     instances: HashMap<Channel, Vec<Handle<AudioInstance>>>,
     channels: HashMap<Channel, ChannelState>,
 }
